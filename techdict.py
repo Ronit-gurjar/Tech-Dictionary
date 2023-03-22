7c6d75f0d41bb507387ac22ccd4b9131ac8f2819
Ronit Gurjar <ronitgjcarl0ver.com>
2023-03-22 23:05:06 +0530

    accessing the dictionary

--- techdict.py
+++ techdict.py
@@ -1,17 +1,22 @@
 import csv 
 
-"""functions"""
+word_dict = dict()
 
+"""functions"""
 #for list to dictionary
 def convert(a):
     it = iter(a)
-    res_dct = dict(zip(it,it))
-    return res_dct
+    res_dict = dict(zip(it,it))
+    word_dict.update(res_dict)
+    return res_dict
 
 """main!!!!!!!"""
 with open('word_dict.csv','r',encoding="utf-8")  as dict_file:
     dict_read = csv.reader(dict_file)
-    next(dict_read)
+    next(dict_read) #skiping the first line
     for line in dict_read:
-        print(convert(line))
-        
+        convert(line)
+
+#accessing the dictionary
+for a in word_dict:
+    print(a,"=",word_dict[a])
